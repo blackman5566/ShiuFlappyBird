010a4d9e61f0be2fbb20aa2c718929e6503800c8
Allen_shiu <pt7922310@gmail.com>
2016-01-26 21:59:24 +0800

    Initial Commit

---
+++ ShiuFlappyBird/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShiuFlappyBird
+//
+//  Created by 許佳豪 on 2016/1/26.
+//  Copyright © 2016年 許佳豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
